14282fae7f828c7074db1ee47b30b969ef014aa5
Jasmine <jasminetntu@gmail.com>
2025-12-14 19:25:13 -0800

    fix bug 1, price filters (#8)

--- api/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ api/src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -21,10 +21,12 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
     public List<Product> search(Integer categoryId, BigDecimal minPrice, BigDecimal maxPrice, String subCategory) {
         List<Product> products = new ArrayList<>();
 
-        String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (subcategory = ? OR ? = '') ";
+        String sql = """
+                SELECT * FROM products
+                WHERE (category_id = ? OR ? = -1)
+                   AND (price >= ? OR ? = -1)
+                   AND (price <= ? OR ? = -1)
+                   AND (subcategory = ? OR ? = '');""";
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
@@ -37,8 +39,10 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, subCategory);
-            statement.setString(6, subCategory);
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, subCategory);
+            statement.setString(8, subCategory);
 
             ResultSet row = statement.executeQuery();
 
@@ -58,8 +62,9 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
     public List<Product> listByCategoryId(int categoryId) {
         List<Product> products = new ArrayList<>();
 
-        String sql = "SELECT * FROM products " +
-                    " WHERE category_id = ? ";
+        String sql = """
+                    SELECT * FROM products
+                    WHERE category_id = ?;""";
 
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
@@ -81,7 +86,7 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
 
     @Override
     public Product getById(int productId) {
-        String sql = "SELECT * FROM products WHERE product_id = ?";
+        String sql = "SELECT * FROM products WHERE product_id = ?;";
 
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
@@ -101,8 +106,10 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
 
     @Override
     public Product create(Product product) {
-        String sql = "INSERT INTO products(name, price, category_id, description, subcategory, image_url, stock, featured) " +
-                " VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        String sql = """
+                INSERT INTO products
+                (name, price, category_id, description, subcategory, image_url, stock, featured)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?);""";
 
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
@@ -138,16 +145,17 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
 
     @Override
     public void update(int productId, Product product) {
-        String sql = "UPDATE products" +
-                " SET name = ? " +
-                "   , price = ? " +
-                "   , category_id = ? " +
-                "   , description = ? " +
-                "   , subcategory = ? " +
-                "   , image_url = ? " +
-                "   , stock = ? " +
-                "   , featured = ? " +
-                " WHERE product_id = ?;";
+        String sql = """
+                UPDATE products
+                SET name = ?,
+                    price = ?,
+                    category_id = ?,
+                    description = ?,
+                    subcategory = ?,
+                    image_url = ?,
+                    stock = ?,
+                    featured = ?
+                WHERE product_id = ?;""";
 
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
@@ -170,8 +178,7 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao {
 
     @Override
     public void delete(int productId) {
-        String sql = "DELETE FROM products " +
-                " WHERE product_id = ?;";
+        String sql = "DELETE FROM products WHERE product_id = ?;";
 
         try (Connection connection = getConnection()) {
             PreparedStatement statement = connection.prepareStatement(sql);
